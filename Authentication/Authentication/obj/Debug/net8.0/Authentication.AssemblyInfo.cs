834df568c7b5dc13e22714e7025857cb1d63ced5
tienthinh19 <thinhbui1907@gmail.com>
2024-06-29 15:25:04 +0700

    update templete

--- Authentication/Authentication/obj/Debug/net8.0/Authentication.AssemblyInfo.cs
+++ Authentication/Authentication/obj/Debug/net8.0/Authentication.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Authentication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce404e1169c0b8ab847b273b13715b134645f368")]
 [assembly: System.Reflection.AssemblyProductAttribute("Authentication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Authentication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
